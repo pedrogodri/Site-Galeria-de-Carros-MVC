ed7ca9e76536722d54a059919975a0f11740d21a
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-11-24 21:48:31 -0300

    Commit - Add Controllers

---
+++ SlnGaleriaCarros/src/Galeria.Carros/Controllers/GaleriaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Galeria.Carros.Controllers
+{
+    public class GaleriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SlnGaleriaCarros/src/Galeria.Carros/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Galeria.Carros.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Galeria.Carros.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("home/privacy")]
+        [Route("privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("home/contato")]
+        [Route("contato")]
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
